36f5e9d3240e82437e1b0444ac677ae371a4cc6b
SHIVAM0001-FLEX <80771521+SHIVAM0001-FLEX@users.noreply.github.com>
2021-04-18 15:21:33 +0530

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,19 +1,37 @@
 const { MessageEmbed } = require("discord.js");
-const { LOCALE } = require("../util/EvobotUtil");
-const i18n = require("i18n");
 
-i18n.setLocale(LOCALE);
 
 module.exports = {
   name: "help",
   aliases: ["h"],
-  description: i18n.__("help.description"),
+  description: ("help.description"),
   execute(message) {
     let commands = message.client.commands.array();
 
     let helpEmbed = new MessageEmbed()
-      .setTitle(i18n.__mf("help.embedTitle", { botname: message.client.user.username }))
-      .setDescription(i18n.__("help.embedDescription"))
+      .setTitle("help.embedTitle", { botname: message.client.user.username }))
+      .setDescription(">help [command] ~ To show all commands
+>join [joins the voice channel]
+>invite [invite] ~ To add/invite the bot to your server
+>dc Leave ~ Leave The Voice Channel!
+>loop loop ~ Toggle music loop
+>lyrics [lyrics] ~ Get lyrics for the currently playing song
+>nowplaying  ~ To show the music which is currently playing in this server
+>pause [pause] ~ To pause the current music in the server
+>play <YouTube_URL> | <song_name> ~ To play songs :D
+>playlist <YouTube Playlist URL | Playlist Name> ~ To play songs :D
+>queue  ~ To show the server songs queue
+>remove rm <number> ~ Remove song from the queue
+>resume  ~ To resume the paused music
+>search <song_name> ~ To search songs :D
+>shuffle [shuffle] ~ Shuffle queue
+>skip  ~ To skip the current music
+>skipto skipto <number> ~ Skip to the selected queue number
+>stop  ~ To stop the music and clearing the queue
+>volume [volume] ~ To change the server song queue volume
+>24/7 [stays 24/7  in voice channel] 
+>feed [feedback for the cloudz]
+")
       .setColor("#F8AA2A");
 
     commands.forEach((cmd) => {
